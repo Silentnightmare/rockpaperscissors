82ac0ef0b13bc30c4a97c774cc2db5c94cde53c8
Name <alecli2014@gmail.com>
2019-11-20 02:33:43 +0000

    redid lost work. added randomizer for computer

--- js/script.js
+++ js/script.js
@@ -1,9 +1,29 @@
 // *************************** YOUR CODE BELOW *******************************
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
+var computerChoice;
+var userChoice;
+var winner;
+
+//based on if randomNumber is greater than .6666, if randomNumber is less than .6666 but greater than .3333 else 
+var randomNumber = Math.random()
 
 //GLOBAL VARIABLES
 /* global $ */
+$('#shoot').click(function() {
+   var userChoice = $('#input').val()
+    $('#userChoice').text(userChoice)
+
+    if (randomNumber > .66666 ) {
+        $("#computerChoices").text("scissors");
+    }
+    else if (randomNumber > .3333 ) {
+        $("#computerChoices").text("rock");
+    }
+    else if (randomNumber > .1111) {
+        $()
+    }
+});
 
 
 
